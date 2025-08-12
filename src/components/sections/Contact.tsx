837443cd26e8b3c0638fe053f9da5fdf484f341c
AdharshJolly <ajcodesofficial+github@gmail.com>
2025-08-13 02:00:46 +0530

    Refactor imports in Contact.tsx to remove unused components

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Mail, Phone } from "lucide-react";
